cb0c83199380d235a0c4a5bf345497698b9633fe
Vilgeir <vilgeir@gmail.com>
2021-02-25 14:55:40 +0100

    added state to check for ifChecked

--- src/Filter.js
+++ src/Filter.js
@@ -6,24 +6,20 @@ import count from "./data/count.json";
 function Filter() {
   const [input, setInput] = useState("not checked");
   const [check, setCheck] = useState(
-    cpvMain.map((item) => [{ cpv: item.cpv, checked: false }])
+    cpvMain.map((item) => ({ cpv: item.cpv, checked: false }))
   );
   const [data, setData] = useState();
 
   let newCount = Object.values(count)[0];
 
   const handleChange = (e) => {
-    console.log(e.target.value);
-
-    setCheck((prevState) => [
-      ...prevState,
-      [{ cpv: e.target.value, checked: true }],
-    ]);
+    let isChecked = check;
+    isChecked.forEach((item) => {
+      item.cpv === e.target.value && (item.checked = e.target.checked);
+    });
+    setCheck(isChecked);
   };
 
-  const test = () => {
-    cpvMain.map((item) => {});
-  };
   console.log(check);
   //   const myInput = <h1>{input}</h1>;
 
@@ -49,9 +45,6 @@ function Filter() {
               {item.cpvmain} (
               {newCount[item.cpv.substring(0, 2) + "000000"].count})
             </label>
-            {console.log(check[0] ? check[0].cpv : "")}
-            {check[0] ? item.cpv == check[0].cpv ? <h3>Hello</h3> : "" : ""}
-            {/* {check && hello} */}
           </div>
         ))}
       </form>
