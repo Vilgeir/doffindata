94a8c6297814cfe9286c8b2cf3cde26eb6d8cdcf
Vilgeir <vilgeir@gmail.com>
2021-04-09 09:49:21 +0200

    added possibility for asc/desc and date sorting

--- src/pages/DetailedList.jsx
+++ src/pages/DetailedList.jsx
@@ -1,25 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import Filter from '../components/Filter'
-import Card from '../components/Card'
-import { useParams } from 'react-router-dom'
-import structure from '../data/withMainCategories'
-import data from '../data/doffin-bach-default-rtdb-F02_2014-export.json'
+import React, { useEffect, useState } from "react";
+import Filter from "../components/Filter";
+import Card from "../components/Card";
+import { useParams } from "react-router-dom";
+import structure from "../data/withMainCategories";
+import data from "../data/doffin-bach-default-rtdb-F02_2014-export.json";
 
 function DetailedList() {
-  const [checkedCategories, setcheckedCategories] = useState([])
-  const [checkedSubCategory, setcheckedSubCategory] = useState([])
+  const [checkedCategories, setcheckedCategories] = useState([]);
+  const [checkedSubCategory, setcheckedSubCategory] = useState([]);
+  const [sort, setSort] = useState();
 
-  const { category, details, subcategory } = useParams()
+  const { category, details, subcategory } = useParams();
 
   useEffect(() => {
-    subcategory && setcheckedCategories([{ [subcategory]: [] }])
-  }, [])
+    subcategory && setcheckedCategories([{ [subcategory]: [] }]);
+  }, []);
 
-  let arr = ['2020-09-', '2020-10-', '2020-11-']
+  let arr = ["2020-09-", "2020-10-", "2020-11-"];
+
+  let newArray = [];
+
+  const sortTest = Object.entries(data).map((i) => newArray.push(i[1]));
+
+  const sorting = (a, b) => {
+    if (sort == "asc") {
+      return a.tittel > b.tittel ? 1 : -1;
+    } else if (sort == "desc") {
+      return a.tittel < b.tittel ? 1 : -1;
+    } else if (sort == "date") {
+      return a.kunngjoringsdato < b.kunngjoringsdato ? 1 : -1;
+    }
+    return 0;
+  };
+
+  const sortedArray = newArray.sort(sorting);
+
+  const onChange = (event) => {
+    setSort(event.target.value);
+  };
+
+  console.log(sortedArray);
 
   return (
-    <div className='detail-container'>
-      <div className='search'>
+    <div className="detail-container">
+      <div className="search">
         <Filter
           details={details}
           subcategory={subcategory}
@@ -30,7 +54,7 @@ function DetailedList() {
           setcheckedSubCategory={setcheckedSubCategory}
         />
       </div>
-      <div className='info-container'>
+      <div className="info-container">
         {subcategory
           ? structure.map(
               (item) =>
@@ -58,23 +82,26 @@ function DetailedList() {
                     )
                 )
             )}
-        <select>
-          <option value='cpv-sort'>Sorter etter</option>
+        <select onChange={onChange}>
+          <option value="cpv-sort">Sorter etter</option>
+          <option value="asc">ASC</option>
+          <option value="desc">DESC</option>
+          <option value="date">Publisert</option>
         </select>
         <div>
           <h3>CPV:</h3>
           {checkedCategories.map((i) => (
             <>
-              <button className='cpv-button'>{Object.keys(i)[0]}</button>
+              <button className="cpv-button">{Object.keys(i)[0]}</button>
               {Object.values(i)[0].map((item) => (
-                <button className='cpv-button'>{item}</button>
+                <button className="cpv-button">{item}</button>
               ))}
             </>
           ))}
         </div>
 
         {checkedCategories.length > 0
-          ? Object.values(data).map((i) =>
+          ? sortedArray.map((i) =>
               checkedCategories.map((checked) =>
                 Object.values(checked)[0].length > 0
                   ? Object.values(checked)[0].map(
@@ -100,7 +127,7 @@ function DetailedList() {
                     )
               )
             )
-          : Object.values(data).map(
+          : sortedArray.map(
               (i) =>
                 i.cpvnumber.substring(0, 2) === details.substring(0, 2) &&
                 arr.map(
@@ -112,7 +139,7 @@ function DetailedList() {
             )}
       </div>
     </div>
-  )
+  );
 }
 
-export default DetailedList
+export default DetailedList;
