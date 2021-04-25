356d26b5236af48757971434584c488db722c285
evyjeanett <42544219+evyjeanett@users.noreply.github.com>
2021-04-25 13:05:22 +0200

    henter ut riktig data i anbudet

--- src/pages/DetailedInfo.jsx
+++ src/pages/DetailedInfo.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
+import { Link } from 'react-router-dom'
 import { getProcurement } from '../helpers/handleData'
 
 function DetailedInfo({ i }) {
@@ -7,59 +8,83 @@ function DetailedInfo({ i }) {
   const { procurement } = useParams()
 
   useEffect(() => {
-    getProcurement('F02_2014', procurement, setProcurement)
+    getProcurement('anbud', procurement, setProcurement)
     // setProcurement(data.filter((i) => i.tittel === procurement))
   }, [])
 
   return (
-    <div className="info-container">
-      <div className="info-left-bar">
+    <div className='info-container'>
+      <div className='info-left-bar'>
         <p>{procurementData.oppdragsgiver}</p>
-        <h1 className="headline">{procurementData.tittel}</h1>
-        <div className="info-card">
-          <p><b>Publiseringsdato:</b> {procurementData.kunngjoringsdato}</p>
-          <p><b>Tilbudsfrist:</b></p>
-          <p><b>Tildelingskriterie:</b></p>
+        <h1 className='headline'>{procurementData.tittel}</h1>
+        <div className='info-card'>
+          <p>
+            <b>Publiseringsdato:</b> {procurementData.kunngjoringsdato}
+          </p>
+          <p>
+            <b>Tilbudsfrist:</b> {procurementData.tilbudsfrist}
+          </p>
+          <p>
+            <b>Tildelingskriterie:</b> {procurementData.tildelingskriterier}
+          </p>
 
           <p>{procurementData.beskrivelse}</p>
         </div>
-        <div className="info-card">
+        <div className='info-card'>
           <h3>Dokumenter</h3>
           {procurementData.documents
             ? procurementData.documents.map((i, index) => (
-                <a key={index} href={"https://www.mercell.com" + i.url}>
+                <a key={index} href={'https://www.mercell.com' + i.url}>
                   {i.name}
                 </a>
               ))
             : procurementData.url_dokumentasjon}
         </div>
-        <div className="info-card">
+        <div className='info-card'>
           <h3>Behov for sortiment</h3>
-          <p>text</p>
+          {procurementData.tilleggsCPV &&
+            procurementData.tilleggsCPV.map((i) => (
+              <p>
+                {Object.keys(i)} {Object.values(i)}{' '}
+              </p>
+            ))}
         </div>
       </div>
-      <div className="info-right-bar">
-        <button className="button">Meld interesse</button>
-        <div className="headline-div">
-          <h3 className="headline">Kontakt</h3>
-          </div>
-        <p><b>Spørsmålsfrist:</b></p>
-        <p><b>Kontaktperson:</b></p>
-        <p><b>Telefon:</b></p>
-        <p><b>E-post:</b> {procurementData.epost}</p>
-        <div className="headline-div">
-          <h3 className="headline">Innkjøper</h3>
-          </div>
-        <p><b>Oppdragsgiver:</b> {procurementData.oppdragsgiver}</p>
-        <p><b>Org nr.:</b></p>
-        <p>
-            <b>Adresse:</b> {procurementData.adresse}
-            {", "} {procurementData.sted}
-          </p>
-        <div className="headline-div">
-          <h3 className="headline">Nettsted</h3>
+      <div className='info-right-bar'>
+        <button className='button'>Meld interesse</button>
+        <div className='headline-div'>
+          <h3 className='headline'>Kontakt</h3>
         </div>
-        <p>{procurementData.nettside}</p>
+        <p>
+          <b>Spørsmålsfrist:</b> {procurementData.sporsmalsfrist}
+        </p>
+        <p>
+          <b>Kontaktperson:</b> {procurementData.kontaktperson}
+        </p>
+        <p>
+          <b>Telefon:</b> {procurementData.telefon}
+        </p>
+        <p>
+          <b>E-post:</b> {procurementData.epost}
+        </p>
+        <div className='headline-div'>
+          <h3 className='headline'>Innkjøper</h3>
+        </div>
+        <p>
+          <b>Oppdragsgiver:</b> {procurementData.oppdragsgiver}
+        </p>
+        <p>
+          <b>Org nr.:</b> {procurementData.orgnr}
+        </p>
+        <p>
+          <b>Adresse:</b> {procurementData.adresse} {procurementData.postkode}
+          {', '} {procurementData.sted} {procurementData.land}
+        </p>
+        <div className='headline-div'>
+          <h3 className='headline'>Nettsted</h3>
+        </div>
+        <Link to={procurementData.url_oppdragsgiver}>Oppdragsgiver</Link>
+        <Link to={procurementData.url_kjoperprofil}>Kjøperprofil</Link>
       </div>
     </div>
   )
