096c9fd736aa671b891e2ad439ab70eb4073844f
evyjeanett <42544219+evyjeanett@users.noreply.github.com>
2021-04-30 09:39:24 +0200

    fixed styling

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,4 +1,78 @@
+import {
+  faArrowRight,
+  faPlug,
+  faTools,
+  faUserMd,
+  faLaptopCode,
+  faBriefcase,
+  faCity,
+  faAtom,
+  faClipboardList,
+  faShippingFast,
+  faShoppingCart,
+} from '@fortawesome/free-solid-svg-icons'
+
 export const capitalize = (str) => {
   let lower = str.toLowerCase()
   return str.charAt(0).toUpperCase() + lower.slice(1)
+}
+
+export const homeIcons = (icon) => {
+  switch (icon) {
+    case 'IT':
+      return {
+        icon: faLaptopCode,
+        style: { backgroundColor: '#36498a', color: '#81b3ee' },
+      }
+    case 'Bygg og anlegg':
+      return {
+        icon: faTools,
+        style: { backgroundColor: '#d1bcff', color: '#4b00eb' },
+      }
+    case 'Helse':
+      return {
+        icon: faUserMd,
+        style: { backgroundColor: '#c8ffdb', color: '#00840d' },
+      }
+    case 'Varer og utstyr':
+      return {
+        icon: faShoppingCart,
+        style: { backgroundColor: '#fff59c', color: '#efbe0e' },
+      }
+    case 'Tjenester':
+      return {
+        icon: faBriefcase,
+        style: { backgroundColor: '#ffe6cf', color: '#ff8a00' },
+      }
+    case 'Elektronisk utstyr':
+      return {
+        icon: faPlug,
+        style: { backgroundColor: '#c8d4ff', color: '#004393' },
+      }
+    case 'Transport':
+      return {
+        icon: faShippingFast,
+        style: { backgroundColor: '#ffcdca', color: '#e20e00' },
+      }
+    case 'Kontor':
+      return {
+        icon: faClipboardList,
+        style: { backgroundColor: '#c0eee6', color: '#007965' },
+      }
+    case 'Olje og kjemikalier':
+      return {
+        icon: faAtom,
+        style: { backgroundColor: '#f2ceff', color: '#9a00cc' },
+      }
+    case 'Offentlige ytelser':
+      return {
+        icon: faCity,
+        style: { backgroundColor: '#ffd3f8', color: '#d8009b' },
+      }
+    default:
+      return {
+        icon: faAtom,
+        style: { backgroundColor: '#f2ceff', color: '#9a00cc' },
+      }
+  }
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,100 +1,108 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import structure from "../data/withMainCategories";
-import "../index.css";
-import SavedSearch from "../components/SavedSearch";
-import Footer from "../components/Footer";
-import Search from "../components/Search";
-import SearchData from "../components/SearchData";
-import AlgoliaSearch from "../components/AlgoliaSearch";
-import { faArrowRight, faPlug } from "@fortawesome/free-solid-svg-icons";
-import { faTools } from "@fortawesome/free-solid-svg-icons";
-import { faUserMd } from "@fortawesome/free-solid-svg-icons";
-import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
-import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
-import { faCity } from "@fortawesome/free-solid-svg-icons";
-import { faAtom } from "@fortawesome/free-solid-svg-icons";
-import { faClipboardList } from "@fortawesome/free-solid-svg-icons";
-import { faShippingFast } from "@fortawesome/free-solid-svg-icons";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { getData } from "../helpers/handleData";
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import structure from '../data/withMainCategories'
+import '../index.css'
+import SavedSearch from '../components/SavedSearch'
+import Footer from '../components/Footer'
+import Search from '../components/Search'
+import SearchData from '../components/SearchData'
+import AlgoliaSearch from '../components/AlgoliaSearch'
+import { homeIcons } from '../helpers/functions'
+import {
+  faArrowRight,
+  faPlug,
+  faTools,
+  faUserMd,
+  faLaptopCode,
+  faBriefcase,
+  faCity,
+  faAtom,
+  faClipboardList,
+  faShippingFast,
+  faShoppingCart,
+} from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { getData } from '../helpers/handleData'
 
 function Home(props) {
-  const [input, setInput] = useState("");
-  const [searchData, setSearchData] = useState();
+  const [input, setInput] = useState('')
+  const [searchData, setSearchData] = useState()
   const [searchDataDefault, setSearchDataDefault] = useState(
     structure.map((i) => i.main)
-  );
+  )
 
-  const [serverData, setServerData] = useState([]);
+  const [serverData, setServerData] = useState([])
 
   //children.map((it, index) => it.code)  .flat()
 
   // console.log(searchDataDefault);
 
-  let arrrray = [];
-  let mainCpv = "";
+  let arrrray = []
+  let mainCpv = ''
 
   useEffect(() => {
-    getData("F02_2014", "cpv", mainCpv, setServerData);
-  }, []);
+    getData('F02_2014', 'cpv', mainCpv, setServerData)
+  }, [])
 
-  Object.entries(serverData).map((i) => arrrray.push(i));
+  Object.entries(serverData).map((i) => arrrray.push(i))
 
-  console.log(serverData);
+  console.log(serverData)
 
   const updateInput = (input) => {
     const filtered = searchDataDefault.filter((main) => {
       if (isNaN()) {
-        return main.toLowerCase().includes(input.toLowerCase());
+        return main.toLowerCase().includes(input.toLowerCase())
       }
-      return main;
-    });
-    if (input === "") return setInput(input), setSearchData([]);
-    return setInput(input), setSearchData(filtered);
-  };
+      return main
+    })
+    if (input === '') return setInput(input), setSearchData([])
+    return setInput(input), setSearchData(filtered)
+  }
 
   //console.log(searchData);
 
-  const homeIcons = (icon) => {
-    switch (icon) {
-      case "IT":
-        return faLaptopCode;
-      case "Bygg og anlegg":
-        return faTools;
-      case "Helse":
-        return faUserMd;
-      case "Varer og utstyr":
-        return faShoppingCart;
-      case "Tjenester":
-        return faBriefcase;
-      case "Elektronisk utstyr":
-        return faPlug;
-      case "Transport":
-        return faShippingFast;
-      case "Kontor":
-        return faClipboardList;
-      case "Olje og kjemikalier":
-        return faAtom;
-      case "Offentlige ytelser":
-        return faCity;
-      default:
-        return faAtom;
-    }
-  };
+  // const homeIcons = (icon) => {
+  //   switch (icon) {
+  //     case 'IT':
+  //       return faLaptopCode
+  //     case 'Bygg og anlegg':
+  //       return faTools
+  //     case 'Helse':
+  //       return faUserMd
+  //     case 'Varer og utstyr':
+  //       return faShoppingCart
+  //     case 'Tjenester':
+  //       return faBriefcase
+  //     case 'Elektronisk utstyr':
+  //       return faPlug
+  //     case 'Transport':
+  //       return faShippingFast
+  //     case 'Kontor':
+  //       return faClipboardList
+  //     case 'Olje og kjemikalier':
+  //       return faAtom
+  //     case 'Offentlige ytelser':
+  //       return faCity
+  //     default:
+  //       return faAtom
+  //   }
+  // }
 
   return (
-    <div className="errthing">
-      <div className="home-container">
-        <div className="head">
-          <div className="aSearch">
-            <AlgoliaSearch />
+    <div className='errthing'>
+      <div className='home-container'>
+        <div>
+          <div className='home-head'>
+            <div className='aSearch'>
+              <AlgoliaSearch />
+            </div>
+            <SavedSearch />
           </div>
 
           {/* <Search input={input} onChange={updateInput} />
           <SearchData searchData={searchData} /> */}
-          <h3 className="headline">Alle kategorier</h3>
+          <h3 className='headline'>Alle kategorier</h3>
         </div>
       </div>
       <div>
@@ -105,32 +113,37 @@ function Home(props) {
           return null;
         })} */}
       </div>
-      <div class="content-container">
-        <div className="box-container">
+      <div class='content-container'>
+        <div className='box-container'>
           {structure.map((i, index) => (
             <Link
-              className="main-boxes"
+              className='main-boxes'
               to={i.main}
               key={index}
-              id={"main-boxes-" + index}
+              id={'main-boxes-' + index}
             >
-              <div className="homeicon-container">
-                <FontAwesomeIcon id="homeicon-icon" icon={homeIcons(i.main)} />
+              <div
+                style={homeIcons(i.main).style}
+                className='homeicon-container'
+              >
+                <FontAwesomeIcon
+                  id='homeicon-icon'
+                  icon={homeIcons(i.main).icon}
+                />
               </div>
-              <div className="main-box-text">
-                <p className="main-boxes-title">{i.main}</p>
-                <p className="main-boxes-cpv">CPV {i.children[0].code}</p>
+              <div className='main-box-text'>
+                <p className='main-boxes-title'>{i.main}</p>
+                <p className='main-boxes-cpv'>CPV {i.children[0].code}</p>
               </div>
-              <div className="main-boxes-arrow">
-                <FontAwesomeIcon icon={faArrowRight} color="#4DBFAC" />
+              <div className='main-boxes-arrow'>
+                <FontAwesomeIcon icon={faArrowRight} color='#4DBFAC' />
               </div>
             </Link>
           ))}
         </div>
-        <SavedSearch />
       </div>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
