9ebdc48e22e3259340ec4f57bdfeab893acd650d
Vilgeir <vilgeir@gmail.com>
2021-05-06 12:58:13 +0200

    Finished search and css

--- src/components/SavedSearch.jsx
+++ src/components/SavedSearch.jsx
@@ -1,19 +1,19 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Link } from 'react-router-dom'
-import { StateContext } from '../context/StateProvider'
+import React, { useContext, useEffect, useState } from "react";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import { StateContext } from "../context/StateProvider";
 
 function SavedSearch() {
-  const [last, setLast] = useState([])
-  const [openLast, setOpenLast] = useState(false)
-  const [saved, setSaved] = useState([])
-  const [openSaved, setOpenSaved] = useState(false)
-  const { setcheckedCategories } = useContext(StateContext)
+  const [last, setLast] = useState([]);
+  const [openLast, setOpenLast] = useState(false);
+  const [saved, setSaved] = useState([]);
+  const [openSaved, setOpenSaved] = useState(false);
+  const { setcheckedCategories } = useContext(StateContext);
 
   let arr = Object.keys(localStorage).filter(
-    (elem) => elem !== 'ally-supports-cache' && elem !== 'lastSearch'
-  )
+    (elem) => elem !== "ally-supports-cache" && elem !== "lastSearch"
+  );
 
   useEffect(() => {
     arr.map((elem) =>
@@ -21,38 +21,40 @@ function SavedSearch() {
         ...prevstate,
         { [elem]: JSON.parse(localStorage.getItem(elem)) },
       ])
-    )
-  }, [])
+    );
+  }, []);
 
   useEffect(() => {
-    let data = JSON.parse(localStorage.getItem('lastSearch'))
-    let arr = []
+    let data = JSON.parse(localStorage.getItem("lastSearch"));
+    let arr = [];
     data &&
       (arr = [
         data.category,
         data.cpv,
         Object.values(data.checkedCategories).map((i) =>
-          Object.entries(i).flat().join(' - ')
+          Object.entries(i).flat().join(" - ")
         ),
-      ])
+      ]);
 
-    setLast(arr)
-  }, [])
+    setLast(arr);
+  }, []);
 
   const handleClick = (i) => {
     Object.values(i).map(
       (item) =>
         item.checkedCategories && setcheckedCategories(item.checkedCategories)
-    )
-  }
+    );
+  };
+
+  // localStorage.clear();
 
   return (
-    <div className='home-buttons'>
+    <div className="home-buttons">
       <button
-        className='home-button'
+        className="home-button"
         onClick={() => setOpenLast((prev) => !prev)}
       >
-        Siste søk{' '}
+        Siste søk{" "}
         {openLast ? (
           <FontAwesomeIcon icon={faChevronUp} />
         ) : (
@@ -62,17 +64,17 @@ function SavedSearch() {
 
       {openLast && (
         <Link
-          to={'/' + Object.values(last)[0] + '/' + Object.values(last)[1]}
+          to={"/" + Object.values(last)[0] + "/" + Object.values(last)[1]}
           onClick={() => handleClick(last)}
         >
-          {Object.values(last).flat().join(' - ')}
+          {Object.values(last).flat().join(" - ")}
         </Link>
       )}
       <button
-        className='home-button'
+        className="home-button"
         onClick={() => setOpenSaved((prev) => !prev)}
       >
-        Lagrede søk{' '}
+        Lagrede søk{" "}
         {openSaved ? (
           <FontAwesomeIcon icon={faChevronUp} />
         ) : (
@@ -84,7 +86,7 @@ function SavedSearch() {
         saved.map((i, index) => (
           <Link
             to={
-              '/' + Object.values(i)[0].category + '/' + Object.values(i)[0].cpv
+              "/" + Object.values(i)[0].category + "/" + Object.values(i)[0].cpv
             }
             key={index}
             onClick={() => handleClick(i)}
@@ -93,7 +95,7 @@ function SavedSearch() {
           </Link>
         ))}
     </div>
-  )
+  );
 }
 
-export default SavedSearch
+export default SavedSearch;
